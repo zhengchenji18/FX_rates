1c57d01d3c04d3181c2fafb61216e9fb2f6c2321
zhengchenji18 <zhengchenji18@gmail.com>
2022-08-01 18:20:51 -0400

    completed updates on combine, created db file

--- application.py
+++ application.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify,request
 application = Flask(__name__)
+app = application
 
-@application.route('/')
-def hello_world():
-	return 'hello world'
+
+@app.route('/')
+def home():
+	return jsonify(message='FX_rates: I return fx rates')
+
+
+@app.route('/get_rates') 
+def get_rates():
+	date = request.args.get('date')
+
+	if not date:
+		return jsonify(mesage = 'please use format http://localhost:5000/get_rates?date=2022-07-16')
+	else:
+		return jsonify('rates' + date)
+
+
+if __name__ == '__main__':
+	app.run()
--- yahoo_data/combine.py
+++ yahoo_data/combine.py
@@ -4,22 +4,53 @@
 #3. Not worried about holidays, all data source have same number of days
 
 import pandas as pd
+import sqlite3
 
-def combine(curs):
+table_name = 'fx'
+
+def combine(curs: list[str]):
     dfs = []
     columns = ['Date','Currency','Close']
 
     for cur in curs:
-        tmp_df = pd.read_csv(cur+"=x.csv")
+        tmp_df = pd.read_csv(cur + "=x.csv")
         tmp_df['Currency'] = cur
         dfs.append(tmp_df)
 
     df_main = pd.concat(dfs,ignore_index=True)
     df_main = df_main[columns]
-    df_output = df_main.pivot(index='Date',columns='Currency',values='Close')
-    df_output.to_csv('fx.csv')
+    df_main['Date'] = pd.to_datetime(df_main['Date'], format='%Y-%m-%d').dt.strftime('%Y-%m-%d %H:%M:%S')
+    df_main.to_csv(table_name + '.csv',index=False)
 
+    #df_test = df_main.pivot(index='Date',columns='Currency',values='Close')
+    #df_test.to_csv('fx.csv')
+
+
+def convertToDB():
+    df_main = pd.read_csv(table_name + '.csv')
+
+    conn = sqlite3.connect(table_name + '.db')
+    create_sql = 'CREATE TABLE IF NOT EXISTS ' + table_name + ' ('
+    create_sql += 'Date TEXT NOT NULL,'
+    create_sql += 'Currency TEXT NOT NULL,'
+    create_sql += 'Close REAL NOT NULL,'
+    create_sql += 'PRIMARY KEY (Date, Currency)'
+    create_sql += ');'
+
+    print(create_sql)
+    cursor = conn.cursor()
+    cursor.execute(create_sql)
+
+    for irow in df_main.itertuples():
+        insert_values_string = ''.join(['INSERT INTO ', table_name, ' VALUES ('])
+        insert_sql = f"{insert_values_string} '{irow[1]}', '{irow[2]}',{irow[3]} )"
+        print(insert_sql)
+        cursor.execute(insert_sql)
+
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
     curs = ['AUD','CAD','JPY']
     combine(curs)
+    convertToDB()
